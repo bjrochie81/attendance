535b9bccfecf01adff2adb793a36d50d1619cd18
Barrington <bjrochie81@gmail.com>
2020-11-10 23:21:08 -0500

    Update db_con.php

--- db/db_con.php
+++ db/db_con.php
@@ -1,16 +1,16 @@
 <?php 
     // Development connection
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $password = '';
-    $charset = 'utf8mb4';
-    // Remote development connection
-    //$host = 'remotemysql.com';
-    //$db = 'plistVH5gv';
-    //$user = 'plistVH5gv';
-    //$password = 'A23zJM3VyF';
+    //$host = '127.0.0.1';
+    //$db = 'attendance_db';
+    //$user = 'root';
+    //$password = '';
     //$charset = 'utf8mb4';
+    // Remote development connection
+    $host = 'remotemysql.com';
+    $db = 'plistVH5gv';
+    $user = 'plistVH5gv';
+    $password = 'A23zJM3VyF';
+    $charset = 'utf8mb4';
     //$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     //$host = 'remotemysql.com';
     //$db = 'EIwosiDlyq';
